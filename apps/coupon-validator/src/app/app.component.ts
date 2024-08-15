a21264e3de2d23781537bb890cf58cf137d8ea65
MZunairKhan <khanzunair15@outlook.com>
2024-08-15 15:00:26 +0500

    Add Material UI

--- apps/coupon-validator/src/app/app.component.ts
+++ apps/coupon-validator/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { MaterialModule } from './shared/material.module';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, MaterialModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
