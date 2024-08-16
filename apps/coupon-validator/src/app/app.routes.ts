9670c6abe15df2b024fd68ebb550ed4f70206767
MZunairKhan <khanzunair15@outlook.com>
2024-08-16 13:43:43 +0500

    Add routes

--- apps/coupon-validator/src/app/app.routes.ts
+++ apps/coupon-validator/src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+
+    { 
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    { 
+        title: 'Home',
+        path: 'home',
+        loadComponent: () => import('./components/home/home.component')
+            .then(c => c.HomeComponent)
+    },
+    { 
+        title: 'Generate',
+        path: 'generate',
+        loadComponent: () => import('./components/generate-coupon/generate-coupon.component')
+            .then(c => c.GenerateCouponComponent)
+    },
+    { 
+        title: 'View',
+        path: 'view',
+        loadComponent: () => import('./components/view-coupons/view-coupons.component')
+            .then(c => c.ViewCouponsComponent)
+    },
+    { 
+        title: 'Not Found',
+        path: '404',
+        loadComponent: () => import('./components/not-found/not-found.component')
+            .then(c => c.NotFoundComponent)
+    },
+    {
+      path: '**',
+      redirectTo: '404',
+      pathMatch: 'full'
+    }
+
+];
