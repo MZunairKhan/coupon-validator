9df01f0c6fbb01e9ac56532c3b1951550c63b8a7
MZunairKhan <khanzunair15@outlook.com>
2024-08-16 15:55:50 +0500

    Home Component Added

--- apps/coupon-validator/src/app/components/home/home.component.ts
+++ apps/coupon-validator/src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../shared/material.module';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule ,MaterialModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
